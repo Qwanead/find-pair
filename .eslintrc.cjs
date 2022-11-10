6634b25559956f2257541e88a79d70c28835ba0a
Антон <anton@cobalt42.ru>
2022-11-10 14:40:42 +0500

    init: create-vue app

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  overrides: [
+    {
+      files: [
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+      ],
+      extends: [
+        'plugin:cypress/recommended',
+      ],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': ['error', 'ignorePackages', {
+      ts: 'never',
+    }],
+  },
+};
