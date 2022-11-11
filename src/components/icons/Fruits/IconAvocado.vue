d26203f56338ccc207b75c19acc4e940783c12c0
Антон <anton@cobalt42.ru>
2022-11-11 15:34:41 +0500

    feat: add fruit icons

---
+++ src/components/icons/Fruits/IconAvocado.vue
@@ -0,0 +1,45 @@
+<template>
+  <svg width="100%" height="100%" viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
+    <path class="cls-1" d="M73.35,47.14s6.48-30.62-6.22-42.61" />
+    <path id="_Clipping_Path_" class="cls-2" d="M117,103.47a42,42,0,0,1-84,0c0-12,5.49-25.49,15.47-41.72,9.4-15.27,15.28-33.6,26.53-33.6s17.13,18.33,26.53,33.6C111.51,78,117,91.52,117,103.47Z" data-name="&lt;Clipping Path&gt;" />
+    <path id="_Clipping_Path_2" class="cls-3" d="M117,103.47a42,42,0,0,1-84,0c0-12,5.49-25.49,15.47-41.72,9.4-15.27,15.28-33.6,26.53-33.6s17.13,18.33,26.53,33.6C111.51,78,117,91.52,117,103.47Z" data-name="&lt;Clipping Path&gt;" />
+    <ellipse class="cls-4" cx="75" cy="97.31" data-name="&lt;Ellipse&gt;" rx="14.11" ry="24.63" />
+    <ellipse class="cls-5" cx="75" cy="97.31" data-name="&lt;Ellipse&gt;" rx="14.11" ry="24.63" />
+  </svg>
+</template>
+
+<style scoped>
+.cls-1,
+.cls-3,
+.cls-5 {
+  fill: none;
+  stroke: #000;
+}
+
+.cls-1 {
+  stroke-linecap: round;
+  stroke-linejoin: round;
+}
+
+.cls-1,
+.cls-3 {
+  stroke-width: 4px;
+}
+
+.cls-2 {
+  fill: #94c634;
+}
+
+.cls-3,
+.cls-5 {
+  stroke-miterlimit: 10;
+}
+
+.cls-4 {
+  fill: #752f1f;
+}
+
+.cls-5 {
+  stroke-width: 3px;
+}
+</style>
---
+++ src/components/icons/Fruits/fruitIconSet.ts
@@ -0,0 +1,45 @@
+import IconBanana from '@/components/icons/Fruits/IconBanana.vue';
+import IconLimon from '@/components/icons/Fruits/IconLimon.vue';
+import IconCherry from '@/components/icons/Fruits/IconCherry.vue';
+import IconPlum from '@/components/icons/Fruits/IconPlum.vue';
+import IconCoconut from '@/components/icons/Fruits/IconCoconut.vue';
+import IconMango from '@/components/icons/Fruits/IconMango.vue';
+import IconOrange from '@/components/icons/Fruits/IconOrange.vue';
+import IconMelon from '@/components/icons/Fruits/IconMelon.vue';
+import IconWaterMelon from '@/components/icons/Fruits/IconWaterMelon.vue';
+import IconAmoras from '@/components/icons/Fruits/IconAmoras.vue';
+import IconPeach from '@/components/icons/Fruits/IconPeach.vue';
+import IconCacao from '@/components/icons/Fruits/IconCacao.vue';
+import IconMaracuja from '@/components/icons/Fruits/IconMaracuja.vue';
+import IconTamarind from '@/components/icons/Fruits/IconTamarind.vue';
+import IconPineapple from '@/components/icons/Fruits/IconPineapple.vue';
+import IconStrawberry from '@/components/icons/Fruits/IconStrawberry.vue';
+import IconPear from '@/components/icons/Fruits/IconPear.vue';
+import IconApple from '@/components/icons/Fruits/IconApple.vue';
+import IconGrape from '@/components/icons/Fruits/IconGrape.vue';
+import IconAvocado from '@/components/icons/Fruits/IconAvocado.vue';
+
+const fruitIconSet = {
+  banana: IconBanana,
+  limon: IconLimon,
+  cherry: IconCherry,
+  plum: IconPlum,
+  coconut: IconCoconut,
+  mango: IconMango,
+  orange: IconOrange,
+  melon: IconMelon,
+  waterMelon: IconWaterMelon,
+  amoras: IconAmoras,
+  peach: IconPeach,
+  cacao: IconCacao,
+  maracuja: IconMaracuja,
+  tamarind: IconTamarind,
+  pineapple: IconPineapple,
+  strawberry: IconStrawberry,
+  pear: IconPear,
+  apple: IconApple,
+  grape: IconGrape,
+  acocado: IconAvocado,
+} as const;
+
+export default fruitIconSet;
